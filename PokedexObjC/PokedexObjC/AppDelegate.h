8d49df88aa12a39ee10942cfc8e9130e292ba9d8
Nar Kumar <njn.kumar@gmail.com>
2020-01-31 08:04:41 -0800

    Initial Commit

---
+++ PokedexObjC/PokedexObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PokedexObjC
+//
+//  Created by Niranjan Kumar on 1/31/20.
+//  Copyright © 2020 Nar Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PokedexObjC/PokedexObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  PokedexObjC
+//
+//  Created by Niranjan Kumar on 1/31/20.
+//  Copyright © 2020 Nar Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
